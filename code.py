b6c3f4e15e5309247406fff2b3bac1bbdea27a2f
Crypt-Coder <prasanna.karmalkar@gmail.com>
2016-08-14 20:18:23 +0530

    A new feature and a minor bug fix

--- code.py
+++ code.py
@@ -9,8 +9,13 @@ print "Can u guess the names of this Famous T.V. Shows!!!"
 #Currently just supports Famous T.V. Shows
 fp = open("tvshows.txt")
 words = fp.readlines()          #all the words even have a '\n' at the end
+donewith = []
 while playOn:
+    if len(donewith) == len(words): donewith=[]     # check if the all the words are covered
     sel = random.randint(0,len(words))
+    while sel in donewith:
+        sel = random.randint(0,len(words))
+    donewith.append(sel)
     word = words[sel][:-1]              #skip '\n'
     guess = ''
     blanks = 0
@@ -27,7 +32,8 @@ while playOn:
     noOfGuess = 0
     while blanks:
         character = ''
-        while (character==''):  character=raw_input("Enter a character(case insensitive):-")[0]
+        while (character==''):  character=raw_input("Enter a character(case insensitive):-")
+        character = character[:1]
         noOfGuess+=1
         asc = ord(character)
         if (ord('A')<=asc and ord('Z')>=asc) or (ord('a')<=asc and ord('z')>=asc):
