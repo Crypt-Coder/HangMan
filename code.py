fe64e4a3e7e2d1d688b0d77d46d0644215e0642a
Crypt-Coder <prasanna.karmalkar@gmail.com>
2016-08-15 19:18:12 +0530

    Fixed a small bug

--- code.py
+++ code.py
@@ -2,23 +2,8 @@
 import sys
 import random
 import re
-import game
+from game import *
 
-def game(word,guess,blanks):
-    noOfGuess = 0
-    while blanks:
-        character = ''
-        while (character==''):  character=raw_input("Enter a character(case insensitive):-")
-        character = character[:1]
-        noOfGuess+=1
-        asc = ord(character)
-        if (ord('A')<=asc and ord('Z')>=asc) or (ord('a')<=asc and ord('z')>=asc):
-            for i in xrange(0,len(word)):
-                if (character.upper() == word[i] or character.lower() == word[i]) and guess[i]=='_':
-                    guess = guess[:i] + word[i] + guess[i+1:]
-                    blanks-=1
-        print guess
-    return noOfGuess
 
 playOn = True
 print "THE HANGMAN GAME!!!"
